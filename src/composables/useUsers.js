c23dc980dba52d1e2ade64cf12036fee7ca9a095
joelgrod <joelgrod@gmail.com>
2021-09-13 11:09:47 +0100

    useUsers composable function

---
+++ src/composables/useUsers.js
@@ -0,0 +1,41 @@
+import { ref } from "vue"
+import axios from "axios"
+
+const useUsers = () => {
+    const users = ref([])
+    const isLoading = ref(true)
+    const currentPage = ref(1)
+    const errorMsg = ref()
+
+    const getUsers = async (page = 1) => {
+      if (page <= 0) page = 1
+      isLoading.value = true
+      const { data } = await axios.get("https://reqres.in/api/users", {
+        params: { page },
+      });
+
+      if (data.data.length > 0) {
+        users.value = data.data
+        currentPage.value = page
+        errorMsg.value = null
+      } else if (currentPage.value > 0) {
+        errorMsg.value = "No users available"
+      }
+
+      isLoading.value = false
+    }
+
+    getUsers()
+
+    return {
+        users,
+        isLoading,
+        currentPage,
+        errorMsg,
+
+        nextPage: () => getUsers( currentPage.value + 1 ),
+        prevPage: () => getUsers( currentPage.value - 1 )
+    }
+}
+
+export default useUsers
--- src/views/HttpPage.vue
+++ src/views/HttpPage.vue
@@ -19,45 +19,25 @@
 </template>
 
 <script>
-import { ref } from "vue";
-import axios from "axios";
+import useUsers from "../composables/useUsers"
 
 export default {
   setup() {
-    const users = ref([])
-    const isLoading = ref(true)
-    const currentPage = ref(1)
-    const errorMsg = ref()
-
-    const getUsers = async (page = 1) => {
-      if (page <= 0) page = 1
-      isLoading.value = true
-      const { data } = await axios.get("https://reqres.in/api/users", {
-        params: { page },
-      });
-
-      if (data.data.length > 0) {
-        users.value = data.data
-        currentPage.value = page
-        errorMsg.value = null
-      } else if (currentPage.value > 0) {
-        errorMsg.value = "No users available"
-      }
-
-      isLoading.value = false
-    }
-
-    getUsers()
-
+    const { 
+      isLoading, 
+      errorMsg, 
+      users, 
+      currentPage, 
+      nextPage, 
+      prevPage } = useUsers()
+    
     return {
-        users,
-        isLoading,
-        currentPage,
-        errorMsg,
-
-        nextPage: () => getUsers( currentPage.value + 1 ),
-        prevPage: () => getUsers( currentPage.value - 1 )
-
+      isLoading, 
+      errorMsg, 
+      users, 
+      currentPage, 
+      nextPage, 
+      prevPage
     }
   },
 };
